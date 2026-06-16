3f063b8681468961fdb162ea12718baec6774e4f
Adelmo amsp <adelmo.msp@gmail.com>
2026-06-16 20:29:51 -0300

    Projeto treinamento_rust criado

---
+++ treinamento_rust/target/debug/deps/treinamento_rust-9c9b0c34fae328d2.d
@@ -0,0 +1,5 @@
+/home/adelmo/desenvolvimento/project_rust/open_academy_rust/treinamento_rust/target/debug/deps/treinamento_rust-9c9b0c34fae328d2.d: src/main.rs
+
+/home/adelmo/desenvolvimento/project_rust/open_academy_rust/treinamento_rust/target/debug/deps/treinamento_rust-9c9b0c34fae328d2: src/main.rs
+
+src/main.rs:
---
+++ treinamento_rust/target/debug/treinamento_rust.d
@@ -0,0 +1,1 @@
+/home/adelmo/desenvolvimento/project_rust/open_academy_rust/treinamento_rust/target/debug/treinamento_rust: /home/adelmo/desenvolvimento/project_rust/open_academy_rust/treinamento_rust/src/main.rs

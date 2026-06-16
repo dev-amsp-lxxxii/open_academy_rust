302e13664c1f7d357213ce12d9a478bbbb7cdb29
Adelmo amsp <adelmo.msp@gmail.com>
2026-06-16 20:53:26 -0300

    Atualização

--- new_rust/target/debug/deps/new_rust-9376135830272079.d
+++
@@ -1,5 +0,0 @@
-/home/adelmo/desenvolvimento/project_rust/open_academy_rust/new_rust/target/debug/deps/new_rust-9376135830272079.d: src/main.rs
-
-/home/adelmo/desenvolvimento/project_rust/open_academy_rust/new_rust/target/debug/deps/libnew_rust-9376135830272079.rmeta: src/main.rs
-
-src/main.rs:
